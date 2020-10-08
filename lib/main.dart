04b6f23e83e45711e577a588ff6f85b31f24db50
Nayeli Pleskac <pleskac510@gmail.com>
2020-10-07 22:26:00 -0700

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 // headline6 = title
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import './screens/statistic_home_screen.dart';
 import './screens/add_stat_screen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
   @override
